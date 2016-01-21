3ce7bb0a6500c06f0b87422c6b9b6957b779fd0b
Mattias Harrysson <mathar@kth.se>
2016-01-21 17:58:58 +0100

    add description

--- app/src/main/java/se/mharrys/tdw/utils/CreateFromIdStrategy.java
+++ app/src/main/java/se/mharrys/tdw/utils/CreateFromIdStrategy.java
@@ -4,6 +4,9 @@ import se.mharrys.tdw.InitializationException;
 import se.mharrys.tdw.article.Article;
 import se.mharrys.tdw.ArticleFactory;
 
+/**
+ * The responsibility of this class is to encapsulate the creation of article from id.
+ */
 public class CreateFromIdStrategy implements CreateArticleStrategy<Article> {
     private int id;
     private ArticleFactory factory;
